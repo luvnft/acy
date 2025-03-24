06a2dd8fc0ddccbfc8ef09f9aedbd58d0dd1d9be
BLK LUV [org] <info@blkluv.org>
2025-03-24 11:02:07 -0400

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
-// TODO : update about
 
+Copy
 import { Avatar, Button } from "./ui";
-import { Lucide, Simple } from "./icons";
+import { Lucide } from "./icons";
 
 export default () => {
   return (
@@ -18,14 +18,11 @@ export default () => {
       </div>
 
       <div>
-        TREE is the gateway to the WEEDW3W 🌌 metaverse, connecting you to Dispensary subdomain TREES (e.g., thejoint.weedw3w.com).
-        Featuring Z🅰️DDY merch, branded TikTok and IG Z🅰️R (ZA AR) Effects, 👻 Dispensary Phantom wallet address, 🎦 LIVE.
-        flash sales & event dates. Go see the 🧙🏼‍♂️ to onboard your dispensary.
+        TREE is the first page of the WEEDW3W metaverse of portals to branches of Dispensary TREES featuring their Z🅰️DDY merch and 🎦 LIVE.
       </div>
 
       <div className="flex self-stretch justify-end">
         <Button as="a" href="https://t.me/hahznft" target="_blank" aria-label="GitHub">
-          {/* Replace GitHub icon and text with emojis */}
           <span className="text-xl">🧙🏼‍♂️ Wizard of Hahz</span>
         </Button>
       </div>
